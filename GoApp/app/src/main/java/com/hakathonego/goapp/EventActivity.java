ade71fc6a50c8f5c4b9cc3a4172fddccbe52ec33
Artem Gorohov <gorohov.art@gmail.com>
2015-10-25 00:57:53 +0300

    event activity

--- GoApp/app/src/main/java/com/hakathonego/goapp/EventActivity.java
+++ GoApp/app/src/main/java/com/hakathonego/goapp/EventActivity.java
@@ -1,7 +1,23 @@
 package com.hakathonego.goapp;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Артём on 24.10.2015.
  */
-public class EventActivity {
+public class EventActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+        Event event = null;
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            //event = extras.getClass() ;
+        }
+
+    }
 }
