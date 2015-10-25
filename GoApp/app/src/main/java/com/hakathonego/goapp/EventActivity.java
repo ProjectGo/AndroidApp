9560ff6849e9c3b1674379e0cca3f3a9dcc20fc7
Artem Gorohov <gorohov.art@gmail.com>
2015-10-25 03:26:42 +0300

    event activity betaa

--- GoApp/app/src/main/java/com/hakathonego/goapp/EventActivity.java
+++ GoApp/app/src/main/java/com/hakathonego/goapp/EventActivity.java
@@ -27,9 +27,9 @@ public class EventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
-        Event loaded = Parcels.unwrap(this.getIntent().getParcelableExtra("whatToShow"));
-        TextView what = (TextView)findViewById(R.id.what);
-        what.setText(loaded.description);
+        Event event = Parcels.unwrap(this.getIntent().getParcelableExtra("whatToShow"));
+        //TextView what = (TextView)findViewById(R.id.what);
+        //.setText(loaded.description);
 
 
         colors[0] = Color.parseColor("#559966CC");
